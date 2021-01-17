6ceaa82041d34a05e8bd457c5e9b9274c3b6fe12
xTrimy <Mohamed.ashraf881999@gmail.com>
2021-01-17 18:11:49 +0200

    Application design work in progress

---
+++ index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Egycon Trivia</title>
+    <link rel="stylesheet" href="layout/css/badaboom.css">
+    <link rel="stylesheet" href="layout/css/helvetica-font.css">
+    <link rel="stylesheet" href="layout/css/head_line_arabic.css">
+    <link rel="stylesheet" href="layout/css/head_line_english.css">
+    <link rel="stylesheet" href="layout/css/master.css">
+</head>
+<body>
+    <div class="wrapper question-background">
+        
+        <div class="side blue">
+            <div class="team headline">Blue Team</div>
+            <div class="score">
+                <span class="score-number headline">30</span>
+                <div class="score-buttons">
+                    <div class="decrease"></div>
+                    <div class="increase"></div>
+                </div>
+            </div>
+            <div class="help-methods">
+                <div class="search"></div>
+                <div class="audience dark"></div>
+                <div class="new dark"></div>
+                <div class="time"></div>
+            </div>
+        </div>
+        <div class="side red">
+            <div class="team headline">Red Team</div>
+            <div class="score">
+                <span class="score-number headline">30</span>
+                <div class="score-buttons">
+                    <div class="decrease"></div>
+                    <div class="increase"></div>
+                </div>
+            </div>
+            <div class="help-methods">
+                <div class="search dark"></div>
+                <div class="audience"></div>
+                <div class="new"></div>
+                <div class="time dark"></div>
+            </div>
+        </div>
+        <div class="main-body">
+            
+            <div class="home">
+                <div class="heading">Anime game show</div>
+            </div>
+
+            <div class="question text">
+                <div class="question-number">
+                    <span>50</span>
+                </div>
+                <div class="box-container">
+                    <img src="layout/test_images/1.png">
+                </div>
+                <div class="question-container">
+                    <div class="question-box">
+                        <div class="shapes"></div>
+                        <div class="shapes question-character"></div>
+                        <span>إيه هو اسم البرنامج الي كان بيتعرض على sakura tv
+و كان بيتمحور حول تمجيد و تعظيم كيرا؟</span>
+                    </div>
+                </div>
+                <div class="answers-container">
+                    <div class="timer">
+                        60
+                    </div>
+                    <div class="answers">
+                        <div class="right">xxxxxxxxxxx</div> 
+                        <div>dd</div> 
+                        <div>xxxxxxxxxxx</div> 
+                        <div class="selected">xxxxxxxxxxx</div> 
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
